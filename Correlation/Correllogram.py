552991c1b93b79ef8dd370d0fff20eba06444d95
WilliamCHW <1018928631@qq.com>
2019-04-28 13:30:08 +0800

    Add files via upload

---
+++ Correlation/Correllogram.py
@@ -0,0 +1,22 @@
+# Correlogram is used to visually see the correlation metric
+# between all possible pairs of numeric variables in a given dataframe (or 2D array).
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+
+
+
+# Import Dataset
+df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mtcars.csv")
+
+# Plot
+plt.figure(figsize=(12,10), dpi= 80)
+sns.heatmap(df.corr(), xticklabels=df.corr().columns, yticklabels=df.corr().columns, cmap='RdYlGn', center=0, annot=True)
+
+# Decorations
+plt.title('Correlogram of mtcars', fontsize=22)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.show()
